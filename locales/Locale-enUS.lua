8cf72efeb3a34d67027ea01e365997f60105f594
rhyddereh <dmangus@requiemtech.com>
2009-06-04 08:12:34 +0000

    -- Switch to using Curseforge's localization system, feel free to contribute to localizations.

--- locales/Locale-enUS.lua
+++ locales/Locale-enUS.lua
@@ -1,48 +1,8 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("NazScrooge", "enUS", true)
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
 
-L["Lockbox Options"] = true
-L["Keep Percent"] = true
-L["Should I set aside a percent of the money you make?"] = true
-L["Percentage of earnings to set aside."] = true
-L["What percent of earnings should I set aside for you?"] = true
-L["<just the percent>"] = true
-L["Target"] = true
-L["Are you saving to a specific goal amount?"] = true
-L["Target amount."] = true
-L["What is the target amount you are trying to save?"] = true
-L["<number in gold>"] = true
-L["Minimum"] = true
-L["Should I set aside a minimum amount of money?"] = true;
-L["Minimum amount to set aside."] = true
-L["What's the minimum should I set aside for you?"] = true
-L["Maximum"] = true
-L["Should I set aside money after you reach a certain amount?"] = true
-L["Maximum gold to keep available"] = true
-L["What's the max gold amount you want available to spend?"] = true
-L["Verbose Output"] = true
-L["Would you like verbose output each time you make money?"] = true
+local L = LibStub("AceLocale-3.0"):NewLocale("NazScrooge", "enUS", true, debug)
 
-L["Deposit"] = true
-L["What amount would you like to Deposit to your lockbox?"] = true
-L["You now have %s in your lockbox"] = true
-L["You tried to deposit more money than you have! Deposited %s."] = true
-L["Withdraw"] = true
-L["What amount would you like to Withdraw from your lockbox?"] = true
-L["You have %s in your lockbox"] = true
-L["You tried to take out more money than you had saved.  Withrew the balance of %s."] = true
-L["Display"] = true
-L["Display the gold in your lockbox"] = true
-L["You are saving %s per hour"] = true
-
-L["Slash-Command"] = "Scrooge" --Probably doesn't need localized unless there is a better word to use as a slash command
-L["Slash-Command-Short"] = "scr" --Shortened command based on the above
-L["Channel"] = true --Word used in lib-sink-2.0, if you cannot find this I can grab it from the lib directly for this locale
-
-L["You do not have enough money to do that."] = true
-L["Saving %s"] = true
-L['You do not have enough saved, increasing lockbox money by %s'] = true
-L['You do not have enough saved, increasing lockbox money to the minimum %s'] = true
-L['You have reached the maximum amount you want available, increasing lockbox money by %s'] = true
-L["Earned %s, saving %s."] = true
-L["You have reached your goal of %s."] = true
-L["CONGRATULATIONS!!"] = true
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
